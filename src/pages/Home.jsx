d7c2aa2faa99bf42966fd5d36eb6b5eef9551f06
Ramon <ramon_20112011@hotmail.com>
2020-11-17 20:51:31 -0300

    added Banner and folders have been organized

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+
 import Header from '../components/Header'
+import Banner from '../components/Banner'
+import About from '../components/About'
 
 const Home = () => {
     return (
         <div>
             <Header/>
+            <Banner/>
+            <About/>
         </div>
     )
 }
