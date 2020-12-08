0db454dcfd33e5078dbbaf13488bb963242755bf
Letícia Bussinger <leticia.bussinger@hotmail.com>
2020-12-08 20:06:33 -0300

    Added Page 02 Serra

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,8 @@ import Header from '../components/Header'
 import Banner from '../components/Banner'
 import About from '../components/About'
 import Services from '../components/Services'
-<<<<<<< HEAD
 import Portfolio from '../components/Portfolio'
-=======
 import Mission from '../components/Mission'
->>>>>>> 3eb7d25c4ea0c6d97984f6291d061474f034291b
 
 function Home() {
     return (
