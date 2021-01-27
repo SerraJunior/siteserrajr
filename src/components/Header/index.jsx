621bf766ef9da865dd307d8046cece9eb3b00ff0
Caio Brugger <caiobrugger@gmail.com>
2021-01-26 22:32:45 -0300

    responsividade header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,34 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../../assets/LogoSerraJr.png'
+import iconeHeader from '../../assets/lista.png'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 import './style.css'
 
 function Header() {
-    return (
-        <header className='header'>
-            <section className='secaoHeader'>
-                <div>
-                    <img className='LogoSerra' src={logo} alt="Logo" />
-                </div>
 
-                <div className='navContainer'>
-                   
+  const [clicked, setClicked] = useState(false)
 
-                <ul className="LinksList">
-                    
+  function getIcon() {
+    if (clicked == false) {
+      return (
+        <div onClick={() => setClicked(true)} className='IconBar'>
+          <FaBars size={30} />
+        </div>
+      )
+    } else if (clicked == true) {
+      return (
+        <div onClick={() => setClicked(false)} className='IconBar'>
+          <FaTimes size={30} />
+        </div>
+      )
+    }
 
-                    <li className="navItem"><a className="LinkHeader" href="#secaoCapa" > INÍCIO </a></li>
-                    <li className="navItem"><a className="LinkHeader" href="#secaoSobre" > SERRA JR. </a></li>
-                    <li className="navItem"><a className="LinkHeader" href="#secaoServicos"> SERVIÇOS </a></li>
-                    <li className="navItem"><a className="LinkHeader" href="#secaoPortifolio"> PORTIFÓLIO </a></li>
-                    <li className="navItem"><a className="LinkHeader" href="/Blog"> BLOG </a></li>
-                    <li className="navItem"><a className="LinkHeader" href="#secaoContato"> CONTATOS </a></li>
-                    
-                </ul>
-                </div>
-            </section>
-        </header>
-    );
+  }
+
+  return (
+    <header className='header'>
+      <section className='secaoHeader'>
+        <a href='http://localhost:3000'>
+          <img className='LogoSerra' src={logo} alt="Logo" />
+        </a>
+
+        {getIcon()}
+        <div className='navContainer'>
+          <ul className="LinksList">
+            <li className="navItem"><a className="LinkHeader" href="http://localhost:3000#secaoCapa" > INÍCIO </a></li>
+            <li className="navItem"><a className="LinkHeader" href="http://localhost:3000#secaoSobre" > SERRA JR. </a></li>
+            <li className="navItem"><a className="LinkHeader" href="http://localhost:3000#secaoServicos"> SERVIÇOS </a></li>
+            <li className="navItem"><a className="LinkHeader" href="http://localhost:3000#secaoPortifolio"> PORTIFÓLIO </a></li>
+            <li className="navItem"><a className="LinkHeader" href="/Blog"> BLOG </a></li>
+            <li className="navItem"><a className="LinkHeader" href="http://localhost:3000#secaoContato"> CONTATOS </a></li>
+          </ul>
+        </div>
+      </section>
+    </header>
+  );
 }
 
-export default Header;
+export default Header
