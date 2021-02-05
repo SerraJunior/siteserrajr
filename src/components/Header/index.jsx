44bfdf51770d4916b366028474d336cd000aa6d8
Ramon <ramon_20112011@hotmail.com>
2021-02-05 00:14:48 -0300

    routes fixed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,17 +8,16 @@ function Header() {
   return (
     <header className='header'>
       <section className='secaoHeader'>
-        <a href='http://localhost:3000'>
+        <a href='https://serrajr.netlify.app/'>
           <img className='LogoSerra' src={logo} alt="Logo" />
         </a>
         <div className='navContainer'>
           <ul className="LinksList">
-            <li className="navItem"><a className="LinkHeader" href="http://localhost:3000#secaoCapa" > INÍCIO </a></li>
-            <li className="navItem"><a className="LinkHeader" href="http://localhost:3000#secaoSobre" > SERRA JR. </a></li>
-            <li className="navItem"><a className="LinkHeader" href="http://localhost:3000#secaoServicos"> SERVIÇOS </a></li>
-            <li className="navItem"><a className="LinkHeader" href="http://localhost:3000#secaoPortifolio"> PORTIFÓLIO </a></li>
-            <li className="navItem"><a className="LinkHeader" href="/Blog"> BLOG </a></li>
-            <li className="navItem"><a className="LinkHeader" href="http://localhost:3000#secaoContato"> CONTATOS </a></li>
+            <li className="navItem"><a className="LinkHeader" href="https://serrajr.netlify.app/#secaoCapa" > INÍCIO </a></li>
+            <li className="navItem"><a className="LinkHeader" href="https://serrajr.netlify.app/#secaoSobre" > SERRA JR. </a></li>
+            <li className="navItem"><a className="LinkHeader" href="https://serrajr.netlify.app/#secaoServicos"> SERVIÇOS </a></li>
+            <li className="navItem"><a className="LinkHeader" href="https://serrajr.netlify.app/#secaoPortifolio"> PORTIFÓLIO </a></li>
+            <li className="navItem"><a className="LinkHeader" href="https://serrajr.netlify.app/#secaoContato"> CONTATOS </a></li>
           </ul>
         </div>
       </section>
