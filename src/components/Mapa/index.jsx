dbed8bc7b5d068048a5b4d073122198b3e858c7e
Ramon <ramon_20112011@hotmail.com>
2021-02-04 23:32:39 -0300

    map improved

--- src/components/Mapa/index.jsx
+++ src/components/Mapa/index.jsx
@@ -1,33 +1,12 @@
 import React from 'react';
-import GoogleMapReact from 'google-map-react';
-import { FaMapMarkerAlt } from 'react-icons/fa'
+
 import './style.css'
- 
-const IconMarker = () => {
-  return (
-    <div>
-      <FaMapMarkerAlt size={40} color='red' />
-    </div>
-  )
-}
  
 function SimpleMap() {
  
     return (
       <div className="map">
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: 'AIzaSyC0J3XYuvy399Puxnwxizilp0ZgzvfjIU4' }}
-          defaultCenter={{
-            lat: -22.286175,
-            lng: -42.5442775
-          }}
-          defaultZoom={16}
-        >
-          <IconMarker
-            lat={-22.286175}
-            lng={-42.5442775}
-          />
-        </GoogleMapReact>
+       <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.783882903157!2d-42.54428284892711!3d-22.286175021551774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x978a85e8cc82f3%3A0xc2954654bfcf2b7e!2sSerra%20Jr.%20Engenharia!5e0!3m2!1spt-BR!2sbr!4v1612490677442!5m2!1spt-BR!2sbr" width="100%" height="500" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
       </div>
     )
 }
