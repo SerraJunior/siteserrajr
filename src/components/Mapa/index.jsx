467ff31e4a0171ce1a27e1252a7b030ff66c439f
Ramon <ramon_20112011@hotmail.com>
2021-02-05 15:03:55 -0300

    responsive map

--- src/components/Mapa/index.jsx
+++ src/components/Mapa/index.jsx
@@ -6,7 +6,7 @@ function SimpleMap() {
  
     return (
       <div className="map">
-       <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.783882903157!2d-42.54428284892711!3d-22.286175021551774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x978a85e8cc82f3%3A0xc2954654bfcf2b7e!2sSerra%20Jr.%20Engenharia!5e0!3m2!1spt-BR!2sbr!4v1612490677442!5m2!1spt-BR!2sbr" width="100%" height="500" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+       <iframe className='mapFrame' title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3691.783882903157!2d-42.54428284892711!3d-22.286175021551774!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x978a85e8cc82f3%3A0xc2954654bfcf2b7e!2sSerra%20Jr.%20Engenharia!5e0!3m2!1spt-BR!2sbr!4v1612490677442!5m2!1spt-BR!2sbr" frameborder="0"   ></iframe>
       </div>
     )
 }
